71d3cc039fa3f01cdae1f4e9d232a3c6e40e8309
Al Jones <allan.jones@agr.gc.ca>
2014-09-30 10:10:01 -0400

    Update server.sh

--- irods-docker/icat/server.sh
+++ irods-docker/icat/server.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd /var/lib/irods/iRODS/server/bin
-env PATH="$PATH:/var/lib/irods/iRODS/server/bin" ./irodsServer -u
+#env PATH="$PATH:/var/lib/irods/iRODS/server/bin" ./irodsServer -u
+env PATH="$PATH:/var/lib/irods/iRODS/server/bin" \
+ irodsSSLCertificateChainFile=/var/lib/irods/iRODS/server/config/chain.pem \
+ irodsSSLCertificateKeyFile=/var/lib/irods/iRODS/server/config/server.key \
+ irodsSSLDHParamsFile=/var/lib/irods/iRODS/server/config/dhparams.pem \
+ irodsSSLCACertificateFile=/var/lib/irods/iRODS/server/config/chain.pem \
+ irodsSSLVerifyServer=cert \
+ irodsAuthScheme=PAM  ./irodsServer -u
+
